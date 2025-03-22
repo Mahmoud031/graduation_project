7df27fb24b7af03dca4600af8d3813891777eb84
Mahmoud031 <mahmoudrady007@gmail.com>
2025-03-22 23:42:53 +0200

    refactor on_boarding_page_view

--- lib/features/on_boarding/presentation/views/widgets/on_boarding_page_view.dart
+++ lib/features/on_boarding/presentation/views/widgets/on_boarding_page_view.dart
@@ -9,40 +9,22 @@ class OnBoardingPageView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(children: [
-      PageView(children: [
-        PageViewItem(
-          image: AppImages.onBoardingPageOne,
-          title: 'Online Unused Medicine Donation for NGOs',
-        ),
-        PageViewItem(
-          image: AppImages.onBoardingPageTwo,
-          title: 'Bridging Care Gaps with Medicine Donations',
-        ),
-        PageViewItem(
-          image: AppImages.onBoardingPageThree,
-          title: 'Reviving Hope with Unused Medicines',
-        ),
-        PageViewItem(
-          image: AppImages.onBoardingPageFour,
-          title: 'Empowering Communities Through Medicine Recycling',
-        ),
-      ]),
-      Positioned(
-        bottom: 8,
-        left: 145,
-        child: DotsIndicator(
-          dotsCount: 4,
-          decorator: DotsDecorator(
-            color: Colors.grey,
-            activeColor: Colors.white,
-            size: const Size.square(9.0),
-            activeSize: const Size(18.0, 9.0),
-            activeShape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(5.0),
-            ),
-          ),
-        ),
+    return PageView(children: [
+      PageViewItem(
+        image: AppImages.onBoardingPageOne,
+        title: 'Online Unused Medicine Donation for NGOs',
+      ),
+      PageViewItem(
+        image: AppImages.onBoardingPageTwo,
+        title: 'Bridging Care Gaps with Medicine Donations',
+      ),
+      PageViewItem(
+        image: AppImages.onBoardingPageThree,
+        title: 'Reviving Hope with Unused Medicines',
+      ),
+      PageViewItem(
+        image: AppImages.onBoardingPageFour,
+        title: 'Empowering Communities Through Medicine Recycling',
       ),
     ]);
   }
--- lib/features/on_boarding/presentation/views/widgets/on_boarding_view_body.dart
+++ lib/features/on_boarding/presentation/views/widgets/on_boarding_view_body.dart
@@ -1,5 +1,5 @@
+import 'package:dots_indicator/dots_indicator.dart';
 import 'package:flutter/material.dart';
-
 import 'on_boarding_page_view.dart';
 
 class OnBoardingViewBody extends StatelessWidget {
@@ -7,6 +7,24 @@ class OnBoardingViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return OnBoardingPageView();
+    return Stack(children: [
+      OnBoardingPageView(),
+      Positioned(
+        bottom: 8,
+        left: 145,
+        child: DotsIndicator(
+          dotsCount: 4,
+          decorator: DotsDecorator(
+            color: Colors.grey,
+            activeColor: Colors.white,
+            size: const Size.square(9.0),
+            activeSize: const Size(18.0, 9.0),
+            activeShape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(5.0),
+            ),
+          ),
+        ),
+      ),
+    ]);
   }
 }
