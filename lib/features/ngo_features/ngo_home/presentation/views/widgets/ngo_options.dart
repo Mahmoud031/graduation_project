74f5fcaaf4445cbcbeec4196db89c8e81e281b0d
Mahmoud031 <mahmoudrady007@gmail.com>
2025-05-16 21:53:58 +0300

    navigate each container to its page

--- lib/features/ngo_features/ngo_home/presentation/views/widgets/ngo_options.dart
+++ lib/features/ngo_features/ngo_home/presentation/views/widgets/ngo_options.dart
@@ -3,6 +3,10 @@ import 'package:graduation_project/core/utils/app_images.dart';
 import 'package:graduation_project/features/donor_features/find_ngo/presentation/views/find_ngo_view.dart';
 import 'package:graduation_project/features/donor_features/support_center/presentation/views/support_center_view.dart';
 import 'package:graduation_project/features/donor_features/view_transaction/presentation/views/view_transaction_view.dart';
+import 'package:graduation_project/features/ngo_features/Distribution_Management/presentation/views/distribution_management_view.dart';
+import 'package:graduation_project/features/ngo_features/Donations/presentation/views/donations_view.dart';
+import 'package:graduation_project/features/ngo_features/Medicine_inventory/presentation/views/medicine_inventory_view.dart';
+import 'package:graduation_project/features/ngo_features/reports/presentation/views/reports_view.dart';
 
 class NgoOption {
   final String title;
@@ -24,25 +28,25 @@ class NgoOptions {
       title: 'Donations',
       imagePath: AppImages.ngoDonations,
       color: Colors.orangeAccent,
-      route: FindNgoView.routeName,
+      route: DonationsView.routeName,
     ),
     NgoOption(
       title: 'Medicine Inventory',
       imagePath: AppImages.ngoMedicineInventory,
       color: Colors.orangeAccent,
-      route: '/donate-money',
+      route: MedicineInventoryView.routeName,
     ),
     NgoOption(
       title: 'Distribution Management',
       imagePath: AppImages.ngoDistributionManagement,
       color: Colors.orangeAccent,
-      route: ViewTransactionView.routeName,
+      route: DistributionManagementView.routeName,
     ),
     NgoOption(
       title: 'Reports',
       imagePath: AppImages.ngoReports,
       color: Colors.orangeAccent,
-      route: SupportCenterView.routeName,
+      route: ReportsView.routeName,
     ),
   ];
 }
